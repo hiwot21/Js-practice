066acc1d60feb550ad30fa0a484787f95d0177f8
mintwab2012 <86166984+hiwot21@users.noreply.github.com>
2021-11-12 16:43:45 -0500

    update script

--- Day-01/js/script.js
+++ Day-01/js/script.js
@@ -1,1 +1,51 @@
-console.log("mintwab");
+// Assign STRING VALUES ONLY to all of the following variables.
+// Assign an adjective
+let adj1 = "playful";
+// Assign an adjective
+let adj2 = "amazing";
+// Assign an adjective
+let adj3 = "large";
+// Assign an adjective
+let adj4 = "intelligent";
+// Assign an adjective
+let adj5 = "hunting";
+// Assign an adjective
+let adj6 = "intellegent";
+// Assign an adverb
+let adverb = "loudly";
+// Assign a color
+let color = "brown";
+// Assign a noun
+let noun1 = "friend";
+// Assign a noun
+let noun2 = "toy";
+// Assign a noun
+let noun3 = "bone";
+// Assign a noun
+let noun4 = "house";
+// Assign a noun
+let noun5 = "Boykin";
+// Assign a noun
+let noun6 = "body";
+// Assign a number
+let num = "10-15";
+// Assign a plural noun
+let pnoun1 = "shepard";
+
+// Open your console to see the results!
+// SPOILER ALERT: KEEP SCROLLING TO VIEW THE REST OF THE CODE
+
+//Using template literals to plug in the values
+let story = `It has often been said that a dog is a man's best ${noun1}. Dogs are very ${adj1} and can be taught many ${adj2}
+tricks. A dog can be trained to carry a ${noun2} in his mouth.
+If you throw his ${noun3}, he will run and fetch it. Dogs
+will also bark ${adverb} if someone tries to break into your
+${noun4} during the night. One of the most popular canine pets
+today is the ${noun5} Spaniel. Spaniels have curly ${color}
+coats and ${adj3} ears. They also have very ${adj4}
+dispositions and live to be ${num} years old. Other popular dogs
+are ${adj5} Terriers, German ${pnoun1}, and the
+${adj6} Poodle. Every ${noun6} should have a loyal dog.`;
+
+//Console log the completed story
+console.log(story);
